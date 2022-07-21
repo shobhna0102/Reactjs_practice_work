9a49cd5d6a7671a838b10b698fe5016876370639
shobhna0102 <shobhnabaraiya@webcluesinfotech.com>
2022-07-21 19:25:42 +0530

    commit

---
+++ singlepage_app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './component/Navbar';
+import './index.css';
+import Navbar from "./component/Navbar"
+
+ReactDOM.render(<Navbar />, document.getElementById('root'));
