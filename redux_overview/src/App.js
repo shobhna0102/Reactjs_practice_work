22ca7f1d507a10feb53c3a5c314a66dac0a082cb
shobhna0102 <shobhnabaraiya@webcluesinfotech.com>
2022-07-27 18:59:53 +0530

    redux practice commit

---
+++ redux_overview/src/App.js
@@ -0,0 +1,13 @@
+import HomeContainer from "./containers/HomeContainer"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeContainer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ redux_overview/src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            <div>
+                <h1>Home</h1>
+                <div className='cart-wrapper'>
+                    <div className='img-wrapper item'>
+                        <img src="images/pizza.jpg" alt="not found" />
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
